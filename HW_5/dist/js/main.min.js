43d18350dd134e2fceed23844d0d1452d2c0b97b
Gabatawr <gabatawr@gmail.com>
2021-05-29 18:00:47 +0300

    update task1

--- HW_5/dist/js/main.min.js
+++ HW_5/dist/js/main.min.js
@@ -13,27 +13,43 @@ const createElement = (targetElement, [elTag, elClass], cssText) => {
 class Marker {
     constructor(props) {
         this.element = props.element;
+
         this.fullness = props.fullness == undefined ? 100 : props.fullness;
+        this._fullnessElement = createElement(
+            this.element,
+            ['div'],
+            `font-size: 20px; color: #808080;`
+        );
+        this._fullnessRefresh();
+        this.element.appendChild(this._fullnessElement);
 
         this._color = props.color == undefined ? '#000000' : props.color;
 
-        this._currentSpan;
+        this._spanElement;
         this._newSpan();
+    }
+
+    _fullnessRefresh() {
+        this._fullnessElement.textContent = `Fullness: ${this.fullness}%`;
+    }
+    _fullnessVisually() {
+        this._fullnessElement.style.display =
+            this._fullnessElement.style.display == 'none' ? 'block' : 'none';
     }
 
     _newSpan() {
         if (
-            this._currentSpan == undefined ||
-            this._currentSpan.textContent !== ''
+            this._spanElement == undefined ||
+            this._spanElement.textContent !== ''
         ) {
-            this._currentSpan = createElement(
+            this._spanElement = createElement(
                 this.element,
                 ['span'],
                 `display: inline-block; color: ${this._color};`
             );
-            this.element.appendChild(this._currentSpan);
+            this.element.appendChild(this._spanElement);
         } else {
-            this._currentSpan.style.color = this._color;
+            this._spanElement.style.color = this._color;
         }
     }
 
@@ -47,26 +63,27 @@ class Marker {
 
     print(text) {
         for (let i = 0; this.fullness > 0 && i < text.length; i++) {
-            this._currentSpan.textContent += text[i];
+            this._spanElement.textContent += text[i];
             if (text[i] !== ' ') {
                 this.fullness -= 0.5;
-            } else if (this._currentSpan.textContent.length === 1) {
-                this._currentSpan.style.display = 'inline';
-                this._currentSpan.style.color = '';
-                this._newSpan();
             }
         }
+        this._fullnessRefresh();
     }
 }
 
 const t1 = createElement(
     main,
     ['section', 't1'],
-    'padding: 12px; font-size: 32px; text-align: center;'
+    'font-size: 28px; text-align: center;'
 );
 
 let marker = new Marker({
-    element: createElement(t1, ['div', 't1__box'], 'white-space: pre;'),
+    element: createElement(
+        t1,
+        ['div', 't1__box'],
+        'padding: 24px 0; white-space: pre; background-color: #e8e8ff;'
+    ),
 });
 
 const abc = 'abc def  ghi   jklmnopqrstuvwxyz';
